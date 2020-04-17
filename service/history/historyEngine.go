98e049acf97576de0ca23e92fb0cdf73eb3f2768
Bowei Xu <boweixu@uber.com>
2020-04-17 15:47:37 -0700

    Add a todo comment

--- service/history/historyEngine.go
+++ service/history/historyEngine.go
@@ -506,6 +506,7 @@ type signalWithStartArg struct {
 	prevMutableState       execution.MutableState
 }
 
+// TODO: may refactor it in more logic clean way, currently people confuse why startWF doesn't need prevMutableState
 func (e *historyEngineImpl) startWorkflowHelper(
 	ctx context.Context,
 	startRequest *h.StartWorkflowExecutionRequest,
